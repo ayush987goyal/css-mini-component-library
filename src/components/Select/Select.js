3d96a1e3fd31e47048c465dea293f978ff30ea2d
Ayush Goyal <goyalayu@amazon.com>
2021-10-18 14:14:58 +0530

    add select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -3,16 +3,48 @@ import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+// import { getDisplayedValue } from './Select.helpers';
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+  // const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <SelectWrapper value={value} onChange={onChange}>
+        {children}
+      </SelectWrapper>
+      <StyledIcon id="chevron-down" size={24} />
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const SelectWrapper = styled.select`
+  padding: 12px 52px 12px 16px;
+  appearance: none;
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  border: none;
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+  color: inherit;
+`;
+
+const StyledIcon = styled(Icon)`
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  pointer-events: none;
+`;
 
 export default Select;
